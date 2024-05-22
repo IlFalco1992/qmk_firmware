c8ddc1ad336036df4b94311f8d960028562b9379
Francesco Scotti <francesco.scotti92@gmail.com>
2024-05-22 09:30:50 +0200

    S(KC_TAB) on EXTRA layer

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/via/keymap.c
@@ -91,11 +91,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [EXTRA] = LAYOUT_tkl_ansi(
-     KC_ESC,   KC_BRID,      KC_BRIU,             KC_MCTL,      KC_LPAD,      RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,           KC_SNAP,   KC_SIRI,  RGB_MOD,
-     KC_GRV,   KC_1,         KC_2,                KC_3,         KC_4,         KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC, KC_INS,    KC_HOME,  KC_PGUP,
-     KC_TAB,   KC_ESC,       KC_GRAVE,            KC_TILDE,     KC_TAB,       KC_T,     KC_Y,     KC_U,     KC_PIPE,  KC_BSLS,  KC_BSPC,  KC_LBRC,    KC_RBRC,  KC_BSLS, KC_DEL,    KC_END,   KC_PGDN,
-     KC_CAPS,  LALT_T(KC_A), LGUI_T(KC_S),        LCTL_T(KC_D), LSFT_T(KC_F), KC_G,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_ENT,   KC_QUOT,              KC_ENT,
-     KC_LSFT,                TG(NUMBERS_SYMBOLS), KC_X,         KC_C,         KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
+     KC_ESC,   KC_BRID,      KC_BRIU,             KC_MCTL,      KC_LPAD,      RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,           KC_SNAP,   KC_SIRI,  RGB_MOD,
+     KC_GRV,   KC_1,         KC_2,                KC_3,         KC_4,         KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC, KC_INS,    KC_HOME,  KC_PGUP,
+     KC_TAB,   KC_ESC,       KC_GRAVE,            KC_TILDE,     KC_TAB,       S(KC_TAB), KC_Y,     KC_U,     KC_PIPE,  KC_BSLS,  KC_BSPC,  KC_LBRC,    KC_RBRC,  KC_BSLS, KC_DEL,    KC_END,   KC_PGDN,
+     KC_CAPS,  LALT_T(KC_A), LGUI_T(KC_S),        LCTL_T(KC_D), LSFT_T(KC_F), KC_G,      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_ENT,   KC_QUOT,              KC_ENT,
+     KC_LSFT,                TG(NUMBERS_SYMBOLS), KC_X,         KC_C,         KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
      KC_LCTL,  KC_LOPTN,     KC_LCMMD,                               LT(EXTRA, KC_SPACE),                                   KC_RCMMD, KC_ROPTN, MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [NUMBERS_SYMBOLS] = LAYOUT_tkl_ansi(
