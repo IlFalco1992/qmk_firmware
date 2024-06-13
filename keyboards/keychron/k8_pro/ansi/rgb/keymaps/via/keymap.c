c68418c0c45e911e36e37a5e25b460152eebb037
Francesco Scotti <francesco.scotti92@gmail.com>
2024-06-13 14:58:41 +0200

    euro symbol

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/via/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV,   KC_1,       KC_2,                KC_3,    KC_4,      KC_5,       KC_6,          KC_7,         KC_8,        KC_9,     KC_0,     KC_MINS,       KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
      KC_TAB,   KC_1,       KC_2,                KC_3,    KC_4,      KC_5,       KC_6,          KC_7,         KC_8,        KC_9,     KC_0,     KC_MINS,       KC_EQL,   KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
      KC_CAPS,  KC_EXCLAIM, KC_AT,               KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LPRN,  KC_RPRN,  KC_UNDERSCORE,           KC_PLUS,
-     KC_LSFT,              TG(NUMBERS_SYMBOLS), KC_X,    KC_C,      KC_V,       KC_B,          KC_N,         KC_M,        KC_COMM,  KC_DOT,   KC_SLSH,                 KC_RSFT,             KC_UP,
+     KC_LSFT,              TG(NUMBERS_SYMBOLS), KC_X,    KC_C,      KC_EUR,     KC_B,          KC_N,         KC_M,        KC_COMM,  KC_DOT,   KC_SLSH,                 KC_RSFT,             KC_UP,
      KC_LCTL,  KC_LOPTN,   KC_LCMMD,                               LAYER_CLEAR,                                     KC_RCMMD, KC_ROPTN, MO(MAC_FN),    KC_RCTL,  KC_LEFT,  KC_DOWN,   KC_RGHT),
 
 };
